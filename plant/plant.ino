a6de9a2432155c5b7620a675131b204923a2600a
Mason Fisher <sackzombie10@gmail.com>
2020-06-01 17:00:22 -0400

    Got Arduino to read values

--- plant/plant.ino
+++ plant/plant.ino
@@ -1,7 +1,23 @@
+#include <dht.h>
+
+dht dhtlib;
+const int moistureDet = A1;
+const int humidityDet = A2;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int moisture = analogRead(moistureDet);
+  int humidity = dhtlib.read11(humidityDet);
+  if (dhtlib.humidity != -999) { // So we don't get a wrong value during cooldown
+    Serial.print("moisture: ");
+    Serial.print(moisture);
+    Serial.print(" - humidity: ");
+    Serial.print(dhtlib.humidity);
+    Serial.print(" - temperature: ");
+    Serial.println(dhtlib.temperature * 1.8 + 32); // Convert to fahrenheit
+  }
+  delay(500);
 }
