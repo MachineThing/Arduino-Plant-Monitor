6494e64cd9809a60e4962e109944c55d9f60c237
Mason Fisher <sackzombie10@gmail.com>
2020-06-06 22:04:32 -0400

    Migrated to a NodeMCU board, had to use a different DHT library

--- plant/plant.ino
+++ plant/plant.ino
@@ -1,23 +1,25 @@
-#include <dht.h>
+#include <DHTesp.h>
 
-dht dhtlib;
-const int moistureDet = A1;
-const int humidityDet = A2;
+const int moistureDet = D2;
+const int humidityDet = D1;
+DHTesp dht;
 
 void setup() {
   Serial.begin(9600);
+  dht.setup(humidityDet, DHTesp::DHT11);
 }
 
 void loop() {
   int moisture = analogRead(moistureDet);
-  int humidity = dhtlib.read11(humidityDet);
-  if (dhtlib.humidity != -999) { // So we don't get a wrong value during cooldown
+  float humidity = dht.getHumidity();
+  float temperat = dht.getTemperature();
+  if (not isnan(humidity)) { // So we don't get a wrong value during cooldown
     Serial.print("moisture: ");
     Serial.print(moisture);
     Serial.print(" - humidity: ");
-    Serial.print(dhtlib.humidity);
+    Serial.print(humidity);
     Serial.print(" - temperature: ");
-    Serial.println(dhtlib.temperature * 1.8 + 32); // Convert to fahrenheit
+    Serial.println(temperat * 1.8 + 32); // Convert to fahrenheit
   }
   delay(500);
 }
