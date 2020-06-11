0ac9409104c0d34cce0763ff38b0c5b1df60ddfc
Mason Fisher <sackzombie10@gmail.com>
2020-06-11 10:22:28 -0400

    Can now get global IP

--- plant/plant.ino
+++ plant/plant.ino
@@ -13,22 +13,28 @@
 
 const int moistureDet = A0;
 const int humidityDet = D1;
+const int interval = 30000; // 30 seconds
 DHTesp dht;
 
 // Variable to store the HTTP request
 String header;
+
 // Plant status variables
 int moisture;
 float humidity;
 float temperat;
+
 // Time variables, they should be unsigned longs and WILL overflow after 50 days
 unsigned long aimTime;
 unsigned long currentTime = millis();
 unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
+
 // Set the web server port to the HTTP port
 WiFiServer server(80);
+// Global IP address variable
+String GlobalIP;
 
 void setup() {
   Serial.begin(9600);
@@ -41,8 +47,39 @@ void setup() {
     Serial.print(".");
   }
   Serial.println("Connected!");
+  Serial.println("Getting IP Addresses...");
+
+  // Get global IP
+
+  WiFiClient client;
+
+  const char* host = "api.ipify.org";
+  
+  if (client.connect(host, 80)) {
+    client.print(String("GET /") + " HTTP/1.1\r\n"+
+      "Host: " + host + "\r\n" +
+      "Connection: close\r\n" +
+      "\r\n"
+    );
+    while (client.connected() || client.available()) {
+      if (client.available()) {
+        GlobalIP = client.readStringUntil('\n');
+      }
+    }
+    client.stop();
+  } else {
+    client.stop();
+    Serial.println("Connection failed...");
+    while (true) {
+      delay(1000);
+    }
+  }
   Serial.print("Your IP Address: ");
   Serial.println(WiFi.localIP());
+  Serial.print("Gateway IP: ");
+  Serial.println(WiFi.gatewayIP());
+  Serial.print("Global IP Address: ");
+  Serial.println(GlobalIP);
   server.begin();
 
   moisture = analogRead(moistureDet);
@@ -55,7 +92,7 @@ void loop() {
   WiFiClient client = server.available(); // Listen for incoming clients
   currentTime = millis();
 
-  if (currentTime - aimTime > 60000) {
+  if (currentTime - aimTime > interval) {
     moisture = analogRead(moistureDet);
     humidity = dht.getHumidity();
     temperat = dht.getTemperature();
@@ -81,10 +118,11 @@ void loop() {
     unsigned long currentTime = millis();
     if (client.available()) {
       char c = client.read(); // Read a byte
+      Serial.write(c);
       header += c;
       if (c == '\n') { // if the byte is a newline character it probably means that the HTTP request is done.
         if (currentLine.length() == 0) {
-          int countdownTime = 60-floor((currentTime - aimTime)/1000); // To remove the decimal points at the end
+          int countdownTime = (interval/1000)-floor((currentTime - aimTime)/1000); // To remove the decimal points at the end
           
           client.println("HTTP/1.1 200 OK");
           client.println("Content-type:text/html");
